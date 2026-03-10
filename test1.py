ef984af1175f4d027aa92b67b347cf07e4db332b
zhennnn215 <doreen950215@gmail.com>
2026-03-10 12:27:19 +0800

    week3_py

--- test1.py
+++ test1.py
@@ -1,5 +1,11 @@
 def square(y):
-	print(f"{y}的平方是{y**3}")
+	print(f"{y}的平方是{y**2}")
+
+def sum_to_n(n):
+	total = 0
+	for i in range(1, n+1):
+		total += i
+	return total
 
 
 x = int(input("請輸入一個整數:"))
@@ -13,3 +19,5 @@ else:
 		#print(i, end=";")
 		square(i)
 
+	s = sum_to_n(x)
+	print("1+2+3+...+{}的總和是{}".format(x, s))
--- use3.py
+++ use3.py
@@ -1,5 +1,15 @@
 from mis import mis2a,mis2b
 
+from mis import sum_to_n
+
+x = int(input("請輸入一個整數:"))
+
+if x <= 0:
+    print(f"您輸入的值是{x},小於等於0")
+else:
+    s = sum_to_n(x)
+    print(f"1+2+3+...+{x}的總和是{s}")
+
 
 mis2a()
 mis2b()
